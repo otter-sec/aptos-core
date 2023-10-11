32a06beaf4e730e087888ee4bc025d624b4d1411
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2023-10-11 01:36:07 +0000

    improve FakeExecutor perf:
    remove threadpool that's being started for each new instance of
    FakeExecutor since we are fuzzing with single code

--- aptos-move/e2e-tests/src/executor.rs
+++ aptos-move/e2e-tests/src/executor.rs
@@ -110,9 +110,8 @@ pub enum ExecutorMode {
 pub struct FakeExecutor {
     data_store: FakeDataStore,
     event_store: Vec<ContractEvent>,
-    executor_thread_pool: Arc<rayon::ThreadPool>,
+    // executor_thread_pool: Arc<rayon::ThreadPool>,
     block_time: u64,
-    executed_output: Option<GoldenOutputs>,
     trace_dir: Option<PathBuf>,
     rng: KeyGen,
     /// If set, determines whether or not to execute a comparison test with the parallel
@@ -132,18 +131,18 @@ pub enum GasMeterType {
 impl FakeExecutor {
     /// Creates an executor from a genesis [`WriteSet`].
     pub fn from_genesis(write_set: &WriteSet, chain_id: ChainId) -> Self {
-        let executor_thread_pool = Arc::new(
-            rayon::ThreadPoolBuilder::new()
-                .num_threads(num_cpus::get())
-                .build()
-                .unwrap(),
-        );
+        // let executor_thread_pool = Arc::new(
+        //     rayon::ThreadPoolBuilder::new()
+        //         .num_threads(num_cpus::get())
+        //         .build()
+        //         .unwrap(),
+        // );
         let mut executor = FakeExecutor {
             data_store: FakeDataStore::default(),
             event_store: Vec::new(),
-            executor_thread_pool,
+            // executor_thread_pool,
             block_time: 0,
-            executed_output: None,
+            
             trace_dir: None,
             rng: KeyGen::from_seed(RNG_SEED),
             executor_mode: None,
@@ -209,18 +208,17 @@ impl FakeExecutor {
 
     /// Creates an executor in which no genesis state has been applied yet.
     pub fn no_genesis() -> Self {
-        let executor_thread_pool = Arc::new(
-            rayon::ThreadPoolBuilder::new()
-                .num_threads(num_cpus::get())
-                .build()
-                .unwrap(),
-        );
+        // let executor_thread_pool = Arc::new(
+        //     rayon::ThreadPoolBuilder::new()
+        //         .num_threads(num_cpus::get())
+        //         .build()
+        //         .unwrap(),
+        // );
         FakeExecutor {
             data_store: FakeDataStore::default(),
             event_store: Vec::new(),
-            executor_thread_pool,
+            // executor_thread_pool,
             block_time: 0,
-            executed_output: None,
             trace_dir: None,
             rng: KeyGen::from_seed(RNG_SEED),
             executor_mode: None,
@@ -233,7 +231,6 @@ impl FakeExecutor {
         // 'test_name' includes ':' in the names, lets re-write these to be '_'s so that these
         // files can persist on windows machines.
         let file_name = test_name.replace(':', "_");
-        self.executed_output = Some(GoldenOutputs::new(&file_name));
         self.set_tracing(test_name, file_name)
     }
 
@@ -241,7 +238,6 @@ impl FakeExecutor {
         // 'test_name' includes ':' in the names, lets re-write these to be '_'s so that these
         // files can persist on windows machines.
         let file_name = test_name.replace(':', "_");
-        self.executed_output = Some(GoldenOutputs::new_at_path(PathBuf::from(path), &file_name));
         self.set_tracing(test_name, file_name)
     }
 
@@ -464,14 +460,15 @@ impl FakeExecutor {
         &self,
         txn_block: &[SignatureVerifiedTransaction],
     ) -> Result<Vec<TransactionOutput>, VMStatus> {
-        BlockAptosVM::execute_block::<_, NoOpTransactionCommitHook<AptosTransactionOutput, VMStatus>>(
-            self.executor_thread_pool.clone(),
-            txn_block,
-            &self.data_store,
-            usize::min(4, num_cpus::get()),
-            None,
-            None,
-        )
+        // BlockAptosVM::execute_block::<_, NoOpTransactionCommitHook<AptosTransactionOutput, VMStatus>>(
+        //     self.executor_thread_pool.clone(),
+        //     txn_block,
+        //     &self.data_store,
+        //     usize::min(4, num_cpus::get()),
+        //     None,
+        //     None,
+        // )
+        todo!()
     }
 
     pub fn execute_transaction_block(
